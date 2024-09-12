d508d2abd683e9aeef6b2ae85a247bc710cafda2
Balirwa Alvin Daniel <159263723+naughtyug@users.noreply.github.com>
2024-09-12 17:47:14 +0300

    Add files via upload

---
+++ Variables.js
@@ -0,0 +1,19 @@
+// Variables
+var my_name = "Sanyuka Alvin"
+
+//Assign a variable
+height = 5,8;
+
+console.log(my_name)
+
+console.log(height)
+
+// let
+let area = 78.5
+
+let f_name;
+f_name = "BADD"
+
+console.log(area);
+console.log(f_name);
+
