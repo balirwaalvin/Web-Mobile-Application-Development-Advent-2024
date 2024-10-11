b181825989169e8d53adccd0e632875abccf9568
Balirwa Alvin Daniel <159263723+naughtyug@users.noreply.github.com>
2024-10-11 14:26:32 +0300

    IF Statements in Java Script
    In this document are detailed example usage of IF Statement and how they are applied in Java Script

---
+++ IF_Statements.js
@@ -0,0 +1,9 @@
+let temperature = 17;
+if (temperature > 25) {
+    console.log("Its hot");
+}else if (temperature >= 15 && temperature <= 25) {
+        console.log("Its moderate");
+    }
+ else {
+    console.log("Its cold");
+}   
