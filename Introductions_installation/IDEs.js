280671d1f7cb5fd3378fdaac75282498cda368ae
Balirwa Alvin Daniel <159263723+naughtyug@users.noreply.github.com>
2024-10-19 12:43:35 +0300

    INTALLING JAVA SCRIPT
    A detailed guide on installing Java Script and IDEs on your computer onto your Coding journey

---
+++ Introductions_installation/IDEs.js
@@ -0,0 +1,19 @@
+//The Role of an IDE in JavaScript Programming
+//An Integrated Development Environment (IDE) is a powerful toolbox designed to make coding more efficient. 
+//An IDE offers a range of features that streamline the development process. As shown below;
+
+//1. CODE EDITING:
+    //Syntax Highlighting - IDEs color-code your code based on syntax, making it much easier to read and understand. 
+    //Autocompletion - As you type, IDEs suggest code completions, saving you time and reducing errors.
+    // This is especially helpful for remembering function names, object properties, and more programming features.
+   //Code Formatting - IDEs can automatically format your code according to best practices, ensuring consistency and readability.
+
+//2. DEBUGGING:
+
+   //Breakpoints - IDEs let you pause code execution at specific points to inspect variables, step through code line by line, and identify the source of errors.
+   //Variable Inspection - You can easily examine the values of variables at different stages of your program's execution,helping you understand how your code is behaving.
+   //Error Highlighting - IDEs often highlight errors directly in your code as you type, providing immediate feedback and helping you fix issues early on.
+
+//Popular JavaScript IDEs:
+
+  //Visual Studio Code & WebStorm
---
+++ Introductions_installation/Installing Java Script.js
@@ -0,0 +1,28 @@
+//1. Download and Install Visual Studio Code:
+
+   //Go to the official Visual Studio Code website
+   //Download the appropriate installer for your operating system (Windows, macOS, or Linux).
+   //Run the installer and follow the instructions on screen to install VS Code.
+
+//2. Install the Node.js Runtime:
+
+   //Node.js allows you to run JavaScript code outside of a web browser.
+   //Download Node.js from the official website:
+   //Choose the specific version for better stability.
+   //Install Node.js by running the downloaded installer and following the instructions.
+
+//3. Create a Project Folder:
+
+   //Create a new folder on your computer where you'll store your JavaScript project files.
+
+//4. Open Your Project Folder in VS Code:
+
+   //Launch Visual Studio Code.
+   //Click on "File" > "Open Folder" and select the project folder you created in the previous step.
+
+//5. Create Your First JavaScript File:
+
+   //In the VS Code Explorer on the left side, click the "New File" icon (it looks like a page with a plus sign) within your project folder.
+   //Name the file something like "script.js" (make sure it has the ".js" extension).
+
+
