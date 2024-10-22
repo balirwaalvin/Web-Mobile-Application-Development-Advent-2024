3ec6decf323b32aaf032ce912a24df60179770fb
Balirwa Alvin Daniel <159263723+naughtyug@users.noreply.github.com>
2024-10-22 14:41:44 +0300

    VARIABLE DECLARATION
    How to declare a variable under Java Script all in this file. Feel free to have a sneak peek

---
+++ Variable Declaration.js
@@ -0,0 +1,10 @@
+// Declare a variable to store the name of a student
+let studentName = "BADD";
+
+// Declare a constant to store the total number of students
+const totalStudents = 85;
+
+// Display the student name and total number of students
+console.log("Student Name:", studentName);
+console.log("Total Students:", totalStudents); 
+
